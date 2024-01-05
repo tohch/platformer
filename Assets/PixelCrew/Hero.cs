36f1f22542ea004c4b184ee46c7dc69ec9c15e59
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-05 20:45:51 +0300

    Добавьте файлы проекта.

---
+++ Assets/PixelCrew/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private float _direction;
+    
+    [SerializeField]
+    private float _speed;
+    
+    public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+    private void Update()
+    {
+        if(_direction != 0) 
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + delta;
+            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    private Hero _hero;
+    private void Awake()
+    {
+        _hero = GetComponent<Hero>();
+    }
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        _hero.SetDirection(horizontal);
+        //if (Input.GetAxis("Horizontal"))
+        //{
+
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    _hero.SetDirection(-1);
+        //}
+        //else if (Input.GetKey(KeyCode.D))
+        //{
+        //    _hero.SetDirection(1);
+        //}
+        //else
+        //{
+        //    _hero.SetDirection(0);
+        //}
+    }
+}
