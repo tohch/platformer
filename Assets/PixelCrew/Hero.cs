af242c3a08f15614d9104db2fbea1dd55653221b
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-19 12:51:08 +0300

    LayerCheck 1:05

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -8,6 +8,15 @@ namespace PixelCrew
 
         [SerializeField]
         private float _speed;
+        [SerializeField]
+        private float _jumpSpeed;
+        [SerializeField]
+        private LayerMask _groundLayer;
+        [SerializeField]
+        private float _groundCheckRadius;
+        [SerializeField]
+        private Vector3 _groundCheckPositionDelta;
+
         private Rigidbody2D _rigidbody;
         public void SetDirection(Vector2 direction)
         {
@@ -21,10 +30,24 @@ namespace PixelCrew
         {
             Debug.Log("Say");
         }
-
+        private bool IsGounded()
+        {
+            var hit = Physics2D.CircleCast(transform.position + _groundCheckPositionDelta, _groundCheckRadius, Vector2.down, 0, _groundLayer);
+            return hit.collider != null;
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = IsGounded() ? Color.green : Color.red;
+            Gizmos.DrawSphere(transform.position + _groundCheckPositionDelta, _groundCheckRadius);
+        }
         private void FixedUpdate()
         {
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+            var isJumping = _direction.y > 0;
+            if (isJumping && IsGounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
         }
     }
 }
