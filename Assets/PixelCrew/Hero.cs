e306d0f5275f4602db2f6f0e20254da1c2ee9434
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-19 19:30:23 +0300

    Физика и коллизии Конец

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -32,9 +32,16 @@ namespace PixelCrew
         {
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
             var isJumping = _direction.y > 0;
-            if (isJumping && IsGounded())
+            if (isJumping)
             {
-                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+                if (IsGounded())
+                {
+                    _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+                }
+            }
+            else if (_rigidbody.velocity.y > 0)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
         }
     }
