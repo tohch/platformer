1d45386aba2f5d389de190cb251ce44cc3101456
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-01-27 20:13:06 +0300

    Передвижение через События Play Input - Events

--- Assets/PixelCrew/HeroInputReader.cs
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -9,23 +9,36 @@ namespace PixelCrew
         private HeroInputAction _inputActions;
         private void Awake()
         {
-            _inputActions = new HeroInputAction();
-            _inputActions.Hero.HorizontalMovement.performed += OnHorizontalMovement;
-            _inputActions.Hero.HorizontalMovement.canceled += OnHorizontalMovement;
-            _inputActions.Hero.SaySomething.performed += OnSaySomething;
+            //_inputActions = new HeroInputAction();
+            //_inputActions.Hero.HorizontalMovement.performed += OnHorizontalMovement;
+            //_inputActions.Hero.HorizontalMovement.canceled += OnHorizontalMovement;
+            //_inputActions.Hero.SaySomething.performed += OnSaySomething;
         }
-        private void OnEnable()
-        {
-            _inputActions.Enable();
-        }
-        private void OnHorizontalMovement(InputAction.CallbackContext context)
+        //private void OnEnable()
+        //{
+        //    _inputActions.Enable();
+        //}
+        //private void OnDisable()
+        //{
+        //    _inputActions.Disable();
+        //}
+        public void OnMovement(InputAction.CallbackContext context)
         {
             var direction = context.ReadValue<Vector2>();
             _hero.SetDirection(direction);
         }
-        private void OnSaySomething(InputAction.CallbackContext context)
+        public void OnSay(InputAction.CallbackContext context)
         {
             _hero.SaySomething();
         }
+        //private void OnHorizontalMovement(InputAction.CallbackContext context)
+        //{
+        //    var direction = context.ReadValue<Vector2>();
+        //    _hero.SetDirection(direction);
+        //}
+        //private void OnSaySomething(InputAction.CallbackContext context)
+        //{
+        //    _hero.SaySomething();
+        //}
     }
 }
