0b47ae313af76967c0b0cdf6752c0bc4bb8f364e
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-02-04 21:18:17 +0300

    Count into Hero

---
+++ Assets/HeroCountCoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCountCoin : MonoBehaviour
+{
+    [SerializeField] private float _coinGoldValue;
+    [SerializeField] private string _coinGoldName;
+    [SerializeField] private float _coinSilverValue;
+    [SerializeField] private string _coinSilverName;
+    private float coinSum;
+    public void CoinSum(string tag)
+    {
+        if (tag == _coinGoldName)
+        {
+            coinSum += _coinGoldValue;
+        }
+        if (tag == _coinSilverName)
+        {
+            coinSum += _coinSilverValue;
+        }
+        Debug.Log(coinSum);
+    }
+}
---
+++ Assets/PixelCrew/Components/CountCoin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+namespace PixelCrew
+{
+    public class CountCoin : MonoBehaviour
+    {
+        [SerializeField] private HeroCountCoin _hero;
+
+        public void CoinCount()
+        {
+            _hero.CoinSum(gameObject.tag);
+        }
+    }
+}
+
