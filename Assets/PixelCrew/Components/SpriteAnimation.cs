e1de9c18d2137c24234f6b1ba7a56b38af456da1
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-02-17 12:33:49 +0300

    SpriteAnimation Свой Аниматор для Объектов окружения

---
+++ Assets/PixelCrew/Components/SpriteAnimation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteAnimation : MonoBehaviour
+    {
+        [SerializeField] private int _frameRate;
+        [SerializeField] private bool _loop;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private UnityEvent _onComplete;
+
+        private SpriteRenderer _renderer;
+        private float _secondsPerFrame;
+        private int _currentSpriteIndex;
+        private float _nexFrameTime;
+
+        private bool _isPlaying = true;
+
+        private void Start()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+            _secondsPerFrame = 1f / _frameRate;
+            _nexFrameTime = Time.time + _secondsPerFrame;
+        }
+
+        private void Update()
+        {
+            if (!_isPlaying || _nexFrameTime > Time.time) return;
+
+            if (_currentSpriteIndex >= _sprites.Length)
+            {
+                if (_loop)
+                {
+                    _currentSpriteIndex = 0;
+                }
+                else
+                {
+                    _isPlaying = false;
+                    _onComplete?.Invoke();
+                    return;
+                }
+            }
+            _renderer.sprite = _sprites[_currentSpriteIndex];
+            _nexFrameTime += _secondsPerFrame;
+            _currentSpriteIndex++;
+        }
+    }
+}
