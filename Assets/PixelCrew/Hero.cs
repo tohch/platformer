5e7f015cd138ffecd31d86070789b560729d5f36
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-02-17 20:30:35 +0300

    a1

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -6,8 +6,7 @@ namespace PixelCrew
 {
     public class Hero : MonoBehaviour
     {
-        //animat1
-        //ani1
+        //a1
         [SerializeField] private float _speed;
         [SerializeField] private float _jumpSpeed;
         [SerializeField] private LayerMask _groundLayer;
