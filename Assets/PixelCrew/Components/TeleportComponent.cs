c0a5fbf8ef4334f455836ebca8af4894f952a795
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-02-29 21:59:50 +0300

    Teleport

---
+++ Assets/PixelCrew/Components/TeleportComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Components
+{
+    internal class TeleportComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _destTrasform;
+
+        public void Teleport(GameObject target)
+        {
+            Debug.Log("tel");
+            target.transform.position = _destTrasform.position;
+        }
+    }
+}
