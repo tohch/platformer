e6a214839eea2f5f10ae8e700a77af1b8c0fa886
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-03-18 21:21:54 +0300

    SpriteAnimation Refactoring

--- Assets/PixelCrew/Components/SpriteAnimation.cs
+++ Assets/PixelCrew/Components/SpriteAnimation.cs
@@ -8,76 +8,176 @@ namespace PixelCrew
     [RequireComponent(typeof(SpriteRenderer))]
     public class SpriteAnimation : MonoBehaviour
     {
-        [SerializeField] private int _frameRate;
-        [SerializeField] private bool _loop;
-        [SerializeField] private bool _allowNextClip;
-        [SerializeField] private SubSprites[] _sprites;
-        [SerializeField] private UnityEvent _onComplete;
-
+        [SerializeField] [Range(1, 30)] private int _frameRate = 10;
+        [SerializeField] private UnityEvent<string> _onComplete;
+        [SerializeField] private AnimationClip[] _clips;
 
         private SpriteRenderer _renderer;
-        private float _secondsPerFrame;
-        private int _currentSpriteIndex;
-        private float _nexFrameTime;
-        private int _nextClip;
 
-        private Sprite[] sprites;
+        private float _secPerFrame;
+        private float _nextFrameTime;
+        private int _currentFrame;
+        private bool _isPlaying = true;
+
+        private int _currentClip;
 
         private void Start()
         {
             _renderer = GetComponent<SpriteRenderer>();
-        }
+            _secPerFrame = 1f / _frameRate;
 
-        private void OnEnable()
-        {
-            _nextClip = 0;
-            _secondsPerFrame = 1f / _frameRate;
-            _nexFrameTime = Time.time + _secondsPerFrame;
-            _currentSpriteIndex = 0;
+            StartAnimation();
         }
-        private void Update()
+        
+        private void OnBecameVisible()
         {
-            sprites = _sprites[_nextClip].sprites;
-            if (_nexFrameTime > Time.time) return;
-
-            if (_currentSpriteIndex >= sprites.Length)
-            {
-                if (_loop)
+            enabled = _isPlaying;
+        }
+        private void OnBecameInvisible()
+        {
+            enabled = false;
+        }
+        public void SetClip(string clipName)
+        {
+            for (var i = 0; i < _clips.Length; i++) 
+            { 
+                if (_clips[i].Name == clipName)
                 {
-                    _currentSpriteIndex = 0;
-                }
-                else
-                {
-                    enabled = false;
-                    _onComplete?.Invoke();
+                    _currentClip = i;
+                    StartAnimation();
                     return;
                 }
             }
 
-            _renderer.sprite = sprites[_currentSpriteIndex];
-            _nexFrameTime += _secondsPerFrame;
-            _currentSpriteIndex++;
-
-            if (_allowNextClip && _currentSpriteIndex >= sprites.Length)
-            {
-                if (_sprites.Length > _nextClip + 1)
-                {
-                    _nextClip++;
-                    _currentSpriteIndex = 0;
-                }
-                else if (_loop)
-                {
-                    _nextClip = 0;
-                    _currentSpriteIndex = 0;
-                }
-            }
+            enabled = _isPlaying = false;
         }
-    }
+        private void StartAnimation()
+        {
+            _nextFrameTime = Time.time + _secPerFrame;
+            _isPlaying = true;
+            _currentFrame = 0;
+        }
 
+        private void OnEnable()
+        {
+            _nextFrameTime = Time.time + _secPerFrame;
+        }
+
+        private void Update()
+        {
+            if (_nextFrameTime > Time.time) return;
+
+            var clip = _clips[_currentClip];
+            if (_currentFrame >= clip.Sprites.Length)
+            {
+                if (clip.Loop)
+                {
+                    _currentFrame = 0;
+                }
+                else
+                {
+                    clip.onComplete?.Invoke();
+                    _onComplete?.Invoke(clip.Name);
+                    enabled = _isPlaying = clip.AllowNextClip;
+                    if (clip.AllowNextClip)
+                    {
+                        _currentFrame = 0;
+                        _currentClip = (int)Mathf.Repeat(_currentClip + 1, _clips.Length);
+                    }
+                }
+                return;
+            }
+
+            _renderer.sprite = clip.Sprites[_currentFrame];
+
+            _nextFrameTime += _secPerFrame;
+            _currentFrame++;
+
+        }
+        //    [SerializeField] [Range(1, 30)] private int _frameRate = 10;
+        //    [SerializeField] private bool _loop;
+        //    [SerializeField] private bool _allowNextClip;
+        //    [SerializeField] private SubSprites[] _sprites;
+        //    [SerializeField] private UnityEvent _onComplete;
+
+
+        //    private SpriteRenderer _renderer;
+        //    private float _secondsPerFrame;
+        //    private int _currentSpriteIndex;
+        //    private float _nexFrameTime;
+        //    private int _nextClip;
+
+        //    private Sprite[] sprites;
+
+        //    private void Start()
+        //    {
+        //        _renderer = GetComponent<SpriteRenderer>();
+        //    }
+
+        //    private void OnEnable()
+        //    {
+        //        _nextClip = 0;
+        //        _secondsPerFrame = 1f / _frameRate;
+        //        _nexFrameTime = Time.time + _secondsPerFrame;
+        //        _currentSpriteIndex = 0;
+        //    }
+        //    private void Update()
+        //    {
+        //        sprites = _sprites[_nextClip].sprites;
+        //        if (_nexFrameTime > Time.time) return;
+
+        //        if (_currentSpriteIndex >= sprites.Length)
+        //        {
+        //            if (_loop)
+        //            {
+        //                _currentSpriteIndex = 0;
+        //            }
+        //            else
+        //            {
+        //                enabled = false;
+        //                _onComplete?.Invoke();
+        //                return;
+        //            }
+        //        }
+
+        //        _renderer.sprite = sprites[_currentSpriteIndex];
+        //        _nexFrameTime += _secondsPerFrame;
+        //        _currentSpriteIndex++;
+
+        //        if (_allowNextClip && _currentSpriteIndex >= sprites.Length)
+        //        {
+        //            if (_sprites.Length > _nextClip + 1)
+        //            {
+        //                _nextClip++;
+        //                _currentSpriteIndex = 0;
+        //            }
+        //            else if (_loop)
+        //            {
+        //                _nextClip = 0;
+        //                _currentSpriteIndex = 0;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //[Serializable]
+        //public class SubSprites
+        //{
+        //    public string name; // дополнительное поле, чтобы в инсекторе отобразить имя массива для удобства
+        //    public Sprite[] sprites;
+    }
     [Serializable]
-    public class SubSprites
+    public class AnimationClip
     {
-        public string name; // дополнительное поле, чтобы в инсекторе отобразить имя массива для удобства
-        public Sprite[] sprites;
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private bool _loop;
+        [SerializeField] private bool _allowNextClip;
+        [SerializeField] private UnityEvent _onComplete;
+        public string Name => _name;
+        public Sprite[] Sprites => _sprites;
+        public bool Loop => _loop;
+        public bool AllowNextClip => _allowNextClip;
+        public UnityEvent onComplete => _onComplete;
     }
 }
