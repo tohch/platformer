94038847d9504308a228d6b4d7362980e4dfdb66
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-04 23:01:24 +0300

    Exit Level

---
+++ Assets/PixelCrew/Components/ExitLevelComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew.Components
+{
+    public class ExitLevelComponent : MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+        public void Exit()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
