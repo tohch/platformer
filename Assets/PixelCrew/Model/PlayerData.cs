776b884493ce676bb697d930fd24e196e9676cd3
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-13 23:00:20 +0300

    session

--- Assets/PixelCrew/Model/PlayerData.cs
+++ Assets/PixelCrew/Model/PlayerData.cs
@@ -9,8 +9,28 @@ namespace PixelCrew.Model
     [Serializable]
     public class PlayerData
     {
-        public int Coins;
-        public int Hp;
-        public bool IsArmed;
+        public int Coins { get => _coins; set => _coins = value; }
+        public int Hp { get => _hp; set => _hp = value; }
+        public bool IsArmed { get => _isArmed; set => _isArmed = value; }
+
+        private int _coins;
+        private int _hp;
+        private bool _isArmed;
+
+        public PlayerData() { }
+        public PlayerData(int coins, int hp, bool isArmed)
+        {
+            Coins = coins;
+            Hp = hp;
+            IsArmed = isArmed;
+        }
+
+        public PlayerData Clone()
+        {
+            return new PlayerData(_coins, _hp, _isArmed);
+        }
+
+        
+
     }
 }
