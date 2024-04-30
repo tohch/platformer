287e2f0d1dcbc131f62b92cf8613bb9e20a45916
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-30 16:08:56 +0300

    Platform Patrol Sharky without wait a second

--- Assets/PixelCrew/Creatures/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/PlatformPatrol.cs
@@ -12,7 +12,6 @@ namespace PixelCrew.Creatures
     public class PlatformPatrol : Patrol
     {
         [SerializeField] private LayerCheck platformCheck;
-        [SerializeField ]private float _waitForTurn = 0.5f;
         private Creature _creature;
         private Vector3 _direction;
 
@@ -23,20 +22,13 @@ namespace PixelCrew.Creatures
 
         public override IEnumerator DoPatrol()
         {
-            bool registerTouchingLayer = platformCheck.IsTouchingLayer;
             var pointIndex = 2f;
             while (enabled)
             {
-                if (!platformCheck.IsTouchingLayer && registerTouchingLayer != platformCheck.IsTouchingLayer)
+                if (!platformCheck.IsTouchingLayer)
                 {
                     _creature.SetDirection(new Vector2(0,0));
                     pointIndex *= -1f;
-                    registerTouchingLayer = !registerTouchingLayer;
-                    yield return new WaitForSeconds(_waitForTurn);
-                }
-                else
-                {
-                    registerTouchingLayer = platformCheck.IsTouchingLayer;
                 }
                 _direction = _creature.transform.position * pointIndex;
                 _direction.y = 0;
