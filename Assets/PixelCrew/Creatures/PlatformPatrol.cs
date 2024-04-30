d9919a46780f223321fc0f354e1bdad2b6357483
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-04-30 15:28:47 +0300

    Platform Patrol Sharky

--- Assets/PixelCrew/Creatures/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/PlatformPatrol.cs
@@ -1,17 +1,48 @@
-﻿using System;
+﻿using PixelCrew.Components;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace PixelCrew.Creatures
 {
     public class PlatformPatrol : Patrol
     {
+        [SerializeField] private LayerCheck platformCheck;
+        [SerializeField ]private float _waitForTurn = 0.5f;
+        private Creature _creature;
+        private Vector3 _direction;
+
+        private void Awake()
+        {
+            _creature = GetComponent<Creature>();
+        }
+
         public override IEnumerator DoPatrol()
         {
-            yield return null;
+            bool registerTouchingLayer = platformCheck.IsTouchingLayer;
+            var pointIndex = 2f;
+            while (enabled)
+            {
+                if (!platformCheck.IsTouchingLayer && registerTouchingLayer != platformCheck.IsTouchingLayer)
+                {
+                    _creature.SetDirection(new Vector2(0,0));
+                    pointIndex *= -1f;
+                    registerTouchingLayer = !registerTouchingLayer;
+                    yield return new WaitForSeconds(_waitForTurn);
+                }
+                else
+                {
+                    registerTouchingLayer = platformCheck.IsTouchingLayer;
+                }
+                _direction = _creature.transform.position * pointIndex;
+                _direction.y = 0;
+                _creature.SetDirection(_direction.normalized);
+                yield return null;
+            }
         }
     }
 }
