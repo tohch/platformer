0ab0e536027c490cbb37768f1fad18b05872d154
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-05-19 21:08:56 +0300

    DropComponent

--- Assets/PixelCrew/Components/LevelManegement/TeleportComponent.cs
+++ Assets/PixelCrew/Components/LevelManegement/TeleportComponent.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.PixelCrew.Components
+namespace PixelCrew.Components.LevelManegement
 {
     internal class TeleportComponent : MonoBehaviour
     {
