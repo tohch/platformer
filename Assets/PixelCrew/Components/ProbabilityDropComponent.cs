e696de94a1424e8c6aa4eb8e749399a3fd1eb74c
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-05-19 20:55:01 +0300

    Random

---
+++ Assets/PixelCrew/Components/ProbabilityDropComponent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Components
+{
+    public class ProbabilityDropComponent : MonoBehaviour
+    {
+        [SerializeField] private int _count;
+        [SerializeField] private DropeData[] _drop;
+        [SerializeField] private DropeEvent _onDropCalculated;
+        [SerializeField] private bool _spawnOnEnable;
+
+        private void OnEnable()
+        {
+            if (_spawnOnEnable)
+            {
+                CalculateDrop();
+            }
+        }
+        [ContextMenu("CalculateDrop")]
+        public void CalculateDrop()
+        {
+            var itemsToDrop = new GameObject[_count];
+            var itemCount = 0;
+            var total = _drop.Sum(dropData => dropData.Probability);
+            var sortedDrop = _drop.OrderBy(dropData => dropData.Probability);
+
+            while(itemCount < _count)
+            {
+                var random = UnityEngine.Random.value * total;
+                var current = 0f;
+                foreach (var dropData in sortedDrop)
+                {
+                    current += dropData.Probability;
+                    if (current >= random)
+                    {
+                        itemsToDrop[itemCount] = dropData.Drop;
+                        itemCount++;
+                        break;
+                    }
+                }
+            }
+
+            _onDropCalculated?.Invoke(itemsToDrop);
+        }
+    }
+
+    [Serializable]
+    class DropeData
+    {
+        public GameObject Drop;
+        [Range(0f, 100f)] public float Probability;
+    }
+    [Serializable]
+    class DropeEvent : UnityEvent<GameObject[]>
+    {
+
+    }
+}
---
+++ Assets/PixelCrew/Components/RandomSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class RandomSpawner : MonoBehaviour
+    {
+        [SerializeField] private float _sectorAngel;
+        [SerializeField] private float _sectorRotaion;
+        [SerializeField] private float _waitTime;
+        [SerializeField] private float _speed;
+
+        public void StartDrop(GameObject[] objectDrop)
+        {
+            for (int i = 0; i < objectDrop.Length; i++)
+            {
+                //int indexItem = GetRandomIndex(_typeItems);
+                var _positionNextItem = gameObject.transform.transform;
+                _positionNextItem.position = new Vector3(_positionNextItem.position.x + 0.2f, _positionNextItem.position.y, _positionNextItem.position.z);
+                var instance = Instantiate(objectDrop[i], _positionNextItem.position, Quaternion.identity);
+            }
+        }
+    }
+}
