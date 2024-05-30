c58adc0527257b46155d4303947de57b4db7d190
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-05-30 21:48:42 +0300

    Sin

--- Assets/PixelCrew/Creatures/Weapons/BaseProjectile.cs
+++ Assets/PixelCrew/Creatures/Weapons/BaseProjectile.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace PixelCrew.Creatures.Weapons
 {
-    public class BaseProjectile
+    public class BaseProjectile : MonoBehaviour
     {
+        [SerializeField] protected float _speed;
+        [SerializeField] private bool _invertX;
+
+        protected int Direction;
+        protected Rigidbody2D Rigidbody;
+
+        protected virtual void Start()
+        {
+            var mod = _invertX ? -1 : 1;
+            Direction = mod * transform.lossyScale.x > 0 ? 1 : -1;
+            Rigidbody = GetComponent<Rigidbody2D>();
+        }
     }
 }
--- Assets/PixelCrew/Creatures/Weapons/SinusoidalProjectile.cs
+++ Assets/PixelCrew/Creatures/Weapons/SinusoidalProjectile.cs
@@ -7,7 +7,25 @@ using UnityEngine;
 
 namespace PixelCrew.Creatures.Weapons
 {
-    public class SinusoidalProjectile : MonoBehaviour
+    public class SinusoidalProjectile : BaseProjectile
     {
+        [SerializeField] private float _frequency = 1f;
+        [SerializeField] private float _amplitude = 1f;
+        private float _originalY;
+        private float _time;
+        protected override void Start()
+        {
+            base.Start();
+            _originalY = Rigidbody.position.y;
+        }
+
+        private void FixedUpdate()
+        {
+            var position = Rigidbody.position;
+            position.x += Direction * _speed;
+            position.y = _originalY + Mathf.Sin(_time * _frequency) * _amplitude;
+            Rigidbody.MovePosition(position);
+            _time += Time.fixedDeltaTime;
+        }
     }
 }
