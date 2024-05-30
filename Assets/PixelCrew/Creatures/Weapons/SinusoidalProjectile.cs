b3028226753120e8f8b6c6cd42b108df0de45312
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-05-30 20:54:45 +0300

    Seashell dead

---
+++ Assets/PixelCrew/Creatures/Weapons/SinusoidalProjectile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PixelCrew.Creatures.Weapons
+{
+    public class SinusoidalProjectile : MonoBehaviour
+    {
+    }
+}
