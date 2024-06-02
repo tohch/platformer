b0ef875a80b4960901d73bebfad06d3073c481e1
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-06-03 00:07:48 +0300

    Ring Coins

---
+++ Assets/PixelCrew/Components/Collectables/RingCoins.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components.Collectables
+{
+    public class RingCoins : MonoBehaviour
+    {
+        private float _angle = 0;
+        private float _radius = 0.5f;
+        private bool _isCircle = true;
+        private float _speed = 1f;
+
+        //private Vector3 _position;
+
+        public void Awake()
+        {
+
+        }
+
+        public void FixedUpdate()
+        {
+            if (_isCircle)
+            {
+                foreach (Transform child in transform)
+                {
+                    var rigidbodyCoin = child.GetComponent<Rigidbody2D>();
+                    var _position = transform.position;
+
+                    _angle += Time.deltaTime;
+
+                    _position.x += Mathf.Cos((_angle * _speed) * _radius);
+                    _position.y += Mathf.Sin((_angle * _speed) * _radius);
+                    rigidbodyCoin.MovePosition(_position);
+                }
+            }
+        }
+    }
+}
