ccad3af1fd4a4680266a6341a1401a5f3e6c6c24
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-07-21 13:55:10 +0300

    The helm shows an opening door by a camera

---
+++ Assets/PixelCrew/Components/CutScens/CameraStateController.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.Components.CutScens
+{
+    public class CameraStateController : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
+        private static readonly int ShowTargetKey = Animator.StringToHash("ShowTarget");
+
+        public void SetPosition(Vector3 targetPosition)
+        {
+            targetPosition.z = _camera.transform.position.z;
+            _camera.transform.position = targetPosition;
+        }
+
+        public void SetState(bool state)
+        {
+            _animator.SetBool(ShowTargetKey, state);
+        }
+    }
+}
---
+++ Assets/PixelCrew/Components/CutScens/ShowTargetComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.Components.CutScens
+{
+    public class ShowTargetComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private CameraStateController _controller;
+        [SerializeField] private float _delay = 0.5f;
+
+        private void OnValidate()
+        {
+            if (_controller == null)
+                _controller = FindObjectOfType<CameraStateController>();
+        }
+
+        private void Awake()
+        {
+            
+        }
+
+        public void ShowTarget()
+        {
+            _controller.SetPosition(_target.position);
+            _controller.SetState(true);
+            Invoke(nameof(MoveBack), _delay);
+        }
+
+        private void MoveBack()
+        {
+            _controller.SetState(false);
+        }
+    }
+}
