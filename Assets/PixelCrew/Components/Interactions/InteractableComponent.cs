2bfc1be7f2b7574e72b2c560e5c8eeebd6dbe8c2
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-07-22 16:58:08 +0300

    reflection

--- Assets/PixelCrew/Components/Interactions/InteractableComponent.cs
+++ Assets/PixelCrew/Components/Interactions/InteractableComponent.cs
@@ -1,4 +1,5 @@
 ﻿using PixelCrew.Creatures;
+using PixelCrew.Utils;
 using System;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,10 +9,32 @@ namespace PixelCrew.Components.Interactions
     internal class InteractableComponent : MonoBehaviour
     {
         [SerializeField] private EnterEvent _action;
+
+        [ContextMenu("Interact")]
         public void Interact(GameObject gameObject = null)
         {
             _action?.Invoke(gameObject);
         }
+
+#if UNITY_EDITOR
+        private GameObject[] _gameObjects;
+
+        private void OnValidate()
+        {
+            _gameObjects = _action.ToGameObjects();
+        }
+
+        public void OnDrawGizmos()
+        {
+            foreach (var go in _gameObjects)
+            {
+                if (go == null) continue;
+
+                Debug.DrawLine(transform.position, go.transform.position, Color.green);
+            }
+        }
+#endif
+
 
         [Serializable]
         public class EnterEvent : UnityEvent<GameObject>
---
+++ Assets/PixelCrew/Utils/UnityEventExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Utils
+{
+    public static class UnityEventExtensions
+    {
+        private const BindingFlags BindingAttr = BindingFlags.NonPublic | BindingFlags.Instance;
+
+        public static GameObject[] ToGameObjects(this UnityEvent<GameObject> unityEvent)
+        {
+            var gos = new List<GameObject>();
+
+            if (unityEvent == null) gos.ToArray();
+
+            // get and check PersistentCalls from UnityEventBase
+            var eventType = typeof(UnityEventBase);
+                
+            var persistentCallsField = eventType.GetField("m_PersistentCalls", BindingAttr);
+            if (persistentCallsField == null) return gos.ToArray();
+            var persistentCallsValue = persistentCallsField.GetValue(unityEvent);
+
+            // get and check List<PersistentCall> from PersistenCalls
+            var callGroupType = persistentCallsValue.GetType();
+            var callGroupField = callGroupType.GetField("m_Calls", BindingAttr);
+            if (callGroupField == null) return gos.ToArray();
+            var callGroupValue = (IEnumerable)callGroupField.GetValue(persistentCallsValue);
+
+            // get and check List<Persistentcall>
+            var listType = callGroupField.GetValue(persistentCallsValue).GetType();
+            if (!listType.IsGenericType || listType.GetGenericTypeDefinition() != typeof(List<>)) return gos.ToArray();
+            var itemType = listType.GetGenericArguments().Single();
+
+            foreach (var pc in callGroupValue)
+            {
+                var itemField = itemType.GetField("m_Target", BindingAttr);
+                if (itemField == null) continue;
+
+                var itemValue = (Object)itemField.GetValue(pc);
+                var propertyInfo = itemValue.GetType().GetProperty("gameObject");
+                if (propertyInfo == null) continue;
+
+                var go = (GameObject)propertyInfo.GetValue(itemValue);
+                gos.Add(go);
+            }
+
+            return gos.ToArray();
+        }
+    }
+}
