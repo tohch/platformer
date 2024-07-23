0313877f075e2b830a72adad22fa11c7301c435d
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-07-23 16:37:18 +0300

    PlatformPatrol

---
+++ Assets/PixelCrew/Components/ColliderBased/ColliderCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.Components.ColliderBased
+{
+    public class ColliderCheck : LayerCheck
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
@@ -7,33 +7,26 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using PixelCrew.Components.ColliderBased;
+using PixelCrew.Creatures.Mobs.Patrolling;
 
 namespace PixelCrew.Creatures.Mobs.Patrolling
 {
     public class PlatformPatrol : Patrol
     {
         [SerializeField] private LayerCheck platformCheck;
-        private Creature _creature;
-        private Vector3 _direction;
-
-        private void Awake()
-        {
-            _creature = GetComponent<Creature>();
-        }
+        [SerializeField] private int _direction;
+        [SerializeField] private Creature _creature;
 
         public override IEnumerator DoPatrol()
         {
-            var pointIndex = 2f;
             while (enabled)
             {
                 if (!platformCheck.IsTouchingLayer)
                 {
-                    _creature.SetDirection(new Vector2(0, 0));
-                    pointIndex *= -1f;
+                    _creature.SetDirection(new Vector2(0,0));
+                    _direction = -_direction;
                 }
-                _direction = _creature.transform.position * pointIndex;
-                _direction.y = 0;
-                _creature.SetDirection(_direction.normalized);
+                _creature.SetDirection(new Vector2(_direction, 0));
                 yield return null;
             }
         }
