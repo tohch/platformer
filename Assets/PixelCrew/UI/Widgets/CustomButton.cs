e7799d3518712ba3a33e7f330515e4563a43a7a6
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-07-30 22:25:43 +0300

    Button

---
+++ Assets/PixelCrew/UI/Widgets/CustomButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Widgets
+{
+    public class CustomButton : Button
+    {
+        [SerializeField] private GameObject _normal;
+        [SerializeField] private GameObject _pressed;
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+
+            _normal.SetActive(state != SelectionState.Pressed);
+            _pressed.SetActive(state == SelectionState.Pressed);
+        }
+    }
+}
---
+++ Assets/PixelCrew/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace PixelCrew.UI.Widgets.Editor
+{
+    [CustomEditor(typeof(CustomButton), true)]
+    [CanEditMultipleObjects]
+    public class CustomButtonEditor : ButtonEditor
+    {
+    }
+}
