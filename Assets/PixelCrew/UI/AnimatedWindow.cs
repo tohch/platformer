3e8c7f38b5a67aeed6d02bb21d5fdb3ed8a13f38
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-07-31 23:12:21 +0300

    Main and Setting Menu

---
+++ Assets/PixelCrew/UI/AnimatedWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PixelCrew.UI
+{
+    public class AnimatedWindow : MonoBehaviour
+    {
+        private Animator _animator;
+        private static readonly int Show = Animator.StringToHash("Show");
+        private static readonly int Hide = Animator.StringToHash("Hide");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+
+            _animator.SetTrigger(Show);
+        }
+
+        public void Close()
+        {
+            _animator.SetTrigger(Hide);
+        }
+
+        public virtual void OnCLoseAnimationComplete()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PixelCrew/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew.UI.MainMenu
+{
+    public class MainMenuWindow : AnimatedWindow
+    {
+        private Action _closeAction;
+        public void OnShowSettings()
+        {
+            var window = Resources.Load<GameObject>("UI/SettingWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+
+        public void OnStartGame()
+        {
+            _closeAction = () => {SceneManager.LoadScene("Level1");};
+            Close();
+        }
+
+        public void OnExit()
+        {
+            _closeAction = () => 
+            {
+                Application.Quit();
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif 
+            };
+            Close();
+        }
+
+        public override void OnCLoseAnimationComplete()
+        {
+            base.OnCLoseAnimationComplete();
+            _closeAction?.Invoke();
+           
+        }
+    }
+}
