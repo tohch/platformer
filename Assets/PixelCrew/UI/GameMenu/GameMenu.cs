facd4c9ee2d75a181bde1af20214cecddcee37a8
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-08-02 23:52:22 +0300

    GameMenu

---
+++ Assets/PixelCrew/UI/GameMenu/GameMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew.UI.GameMenu
+{
+    public class GameMenu : AnimatedWindow
+    {
+        private Action _closeAction;
+
+        public void OnShowSettings()
+        {
+            var window = Resources.Load<GameObject>("UI/SettingWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+        public void OnRestartLevel()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            _closeAction = () => { SceneManager.LoadScene(scene.name); };
+            Close();
+        }
+        public void OnOk()
+        {
+            Close();
+        }
+        public void OnExit()
+        {
+            _closeAction = () =>
+            {
+                Application.Quit();
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif 
+            };
+            Close();
+        }
+        public override void OnCLoseAnimationComplete()
+        {
+            _closeAction?.Invoke();
+            base.OnCLoseAnimationComplete();
+        }
+    }
+}
---
+++ Assets/PixelCrew/UI/GameMenu/ShowerGameMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.UI.GameMenu
+{
+    public class ShowerGameMenu : MonoBehaviour
+    {
+        public void OnShowGameMenu()
+        {
+            var windowInScen = GameObject.Find("GameMenuWindow(Clone)");
+            if (windowInScen == null)
+            {
+                var window = Resources.Load<GameObject>("UI/GameMenuWindow");
+                var canvas = FindObjectOfType<Canvas>();
+                Instantiate(window, canvas.transform);
+            }
+        }    
+
+    }
+}
