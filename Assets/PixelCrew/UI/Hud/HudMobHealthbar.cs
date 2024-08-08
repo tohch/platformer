658179cb5d8edd420b276538aeaab164ee2fb1f2
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-08-08 23:52:52 +0300

    HudMobHealthbar

---
+++ Assets/PixelCrew/UI/Hud/HudMobHealthbar.cs
@@ -0,0 +1,27 @@
+﻿using PixelCrew.Components.Health;
+using PixelCrew.UI.Widgets;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Hud
+{
+    public class HudMobHealthbar : MonoBehaviour
+    {
+        [SerializeField] private HealthComponent _mob;
+        [SerializeField] private ProgressBarWidget _healthbar;
+
+        private float _maxHp;
+
+        private void Start()
+        {
+            _maxHp = (float)_mob.Health;
+        }
+
+        private void Update()
+        {
+            float value = _mob.Health / _maxHp;
+            _healthbar.SetProgress(value);
+        }
+    }
+}
