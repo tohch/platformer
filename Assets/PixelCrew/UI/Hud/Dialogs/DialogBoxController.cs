fe182e51206c5ca836670d4570fa2fb2d69ba5da
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-08-27 21:20:34 +0300

    Dialog box

--- Assets/PixelCrew/UI/Hud/Dialogs/DialogBoxController.cs
+++ Assets/PixelCrew/UI/Hud/Dialogs/DialogBoxController.cs
@@ -1,5 +1,6 @@
 ﻿using PixelCrew.Model.Data.Properties;
 using PixelCrew.Utils;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,9 +20,12 @@ namespace PixelCrew.UI.Hud.Dialogs
         [SerializeField] private AudioClip _open;
         [SerializeField] private AudioClip _close;
 
+        private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+        
         private DialogData _data;
         private int _currentSencence;
         private AudioSource _sfxSource;
+        private Coroutine _typingRoutine;
 
         private void Start()
         {
@@ -36,6 +40,76 @@ namespace PixelCrew.UI.Hud.Dialogs
 
             _container.SetActive(true);
             _sfxSource.PlayOneShot(_open);
+            _animator.SetBool(IsOpen, true);
+        }
+
+        private IEnumerator TypeDialogText()
+        {
+            _text.text = string.Empty;
+            var sentences = _data.Sentences[_currentSencence];
+            foreach(var letter in sentences)
+            {
+                _text.text += letter;
+                _sfxSource.PlayOneShot(_typing);
+                yield return new WaitForSeconds(_textSpeed);
+            }
+
+            _typingRoutine = null;
+        }
+
+        public void OnSkip()
+        {
+            if (_typingRoutine == null) return;
+
+            StopTypeAnimation();
+            _text.text = _data.Sentences[_currentSencence];
+        }
+
+        public void OnContinue()
+        {
+            StopTypeAnimation();
+            _currentSencence++;
+
+            var isDialogCompleted = _currentSencence >= _data.Sentences.Length;
+            if (isDialogCompleted)
+            {
+                HideDialogBox();
+            }
+            else
+            {
+                OnStartDialogAnimation();
+            }
+        }
+
+        private void HideDialogBox()
+        {
+            _animator.SetBool(IsOpen, false);
+            _sfxSource.PlayOneShot(_close);
+        }
+
+        private void StopTypeAnimation()
+        {
+            if (_typingRoutine != null)
+                StopCoroutine(_typingRoutine);
+            _typingRoutine = null;
+        }
+
+        public void OnStartDialogAnimation()
+        {
+            _typingRoutine = StartCoroutine(TypeDialogText());
+        }
+
+
+        public void OnCloseAnimationComplete()
+        {
+
+        }
+
+        [SerializeField] private DialogData _testData;
+
+        public void Test()
+        {
+            ShowDialog(_testData);
         }
     }
 }
