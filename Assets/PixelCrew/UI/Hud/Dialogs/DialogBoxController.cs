9ea23d6935af458a2a47e5cffcea4a494f4c1bae
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-08-27 16:47:42 +0300

    DialogBoxController

---
+++ Assets/PixelCrew/UI/Hud/Dialogs/DialogBoxController.cs
@@ -0,0 +1,41 @@
+﻿using PixelCrew.Model.Data.Properties;
+using PixelCrew.Utils;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Hud.Dialogs
+{
+    public class DialogBoxController : MonoBehaviour
+    {
+
+        [SerializeField] private Text _text;
+        [SerializeField] private GameObject _container;
+        [SerializeField] private Animator _animator;
+
+        [Space] [SerializeField] private float _textSpeed = 0.09f;
+
+        [Header("Sounds")] [SerializeField] private AudioClip _typing;
+        [SerializeField] private AudioClip _open;
+        [SerializeField] private AudioClip _close;
+
+        private DialogData _data;
+        private int _currentSencence;
+        private AudioSource _sfxSource;
+
+        private void Start()
+        {
+            _sfxSource = AudioUtils.FindSfxSource();
+        }
+
+        public void ShowDialog(DialogData data)
+        {
+            _data = data;
+            _currentSencence = 0;
+            _text.text = string.Empty;
+
+            _container.SetActive(true);
+            _sfxSource.PlayOneShot(_open);
+        }
+    }
+}
