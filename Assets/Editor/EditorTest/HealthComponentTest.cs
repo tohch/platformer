c72ee093e62ba0bc9a8bc529b212c5db49c2aa36
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-09-04 22:16:57 +0300

    Test

---
+++ Assets/Editor/EditorTest/HealthComponentTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using PixelCrew.Components.Health;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponentTest
+{
+    GameObject go;
+    HealthComponent healthComponent;
+
+    [SetUp]
+    public void init()
+    {
+        go = new GameObject();
+        healthComponent = go.AddComponent<HealthComponent>();
+    }
+
+    [Test]
+    public void HelthComponentModifyHealthTest()
+    {
+        healthComponent.ModifyHealth(10);
+
+        Assert.AreEqual(0, healthComponent.Health);
+    }
+}
