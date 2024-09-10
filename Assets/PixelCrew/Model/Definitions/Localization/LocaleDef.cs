2a20f335315a4d1cd2e3dc091faf7d156da229df
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-09-10 16:54:54 +0300

    link for localization

---
+++ Assets/PixelCrew/Model/Definitions/Localization/LocaleDef.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.Model.Definitions.Localization
+{
+    public class LocaleDef : ScriptableObject
+    {
+        //en https://docs.google.com/spreadsheets/d/e/2PACX-1vSYMZgu95J7c6emUwvs8enT8quRateiVfB6nJRx-uOiJ2_N5TTDkU3J3NjDxYYqagWZlgZBnV0aAX51/pubhtml?gid=0&single=true
+        //ru https://docs.google.com/spreadsheets/d/e/2PACX-1vSYMZgu95J7c6emUwvs8enT8quRateiVfB6nJRx-uOiJ2_N5TTDkU3J3NjDxYYqagWZlgZBnV0aAX51/pubhtml?gid=2136936092&single=true
+        //es https://docs.google.com/spreadsheets/d/e/2PACX-1vSYMZgu95J7c6emUwvs8enT8quRateiVfB6nJRx-uOiJ2_N5TTDkU3J3NjDxYYqagWZlgZBnV0aAX51/pubhtml?gid=2009360454&single=true
+
+    }
+}
