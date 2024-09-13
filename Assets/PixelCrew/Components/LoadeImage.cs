26e2d75084e87455ecb645c2b943a190a652fa66
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-09-13 10:43:24 +0300

    Image Localization

---
+++ Assets/PixelCrew/Components/LoadeImage.cs
@@ -0,0 +1,42 @@
+﻿using PixelCrew.Model.Definitions.Localization;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.Components
+{
+    public class LoadeImage : MonoBehaviour
+    {
+        [SerializeField] private string _spriteName;
+
+        private string selectedLocale;
+        private Sprite imageCurrentLocale;
+        private SpriteRenderer sprite;
+
+
+        private void Awake()
+        {
+            sprite = GetComponent<SpriteRenderer>();
+            ChangeImage();
+
+        }
+
+        private void Start()
+        {
+            LocalizationManager.I.OnLocaleChanged += ChangeImage;
+        }
+
+        private void ChangeImage()
+        {
+            selectedLocale = LocalizationManager.I.LocaleKey;
+            imageCurrentLocale = Resources.Load<Sprite>($"ImageLocales/{selectedLocale}/{_spriteName}");
+            sprite.sprite = imageCurrentLocale;
+        }
+
+        private void OnDestroy()
+        {
+            LocalizationManager.I.OnLocaleChanged -= ChangeImage;
+        }
+    }
+}
