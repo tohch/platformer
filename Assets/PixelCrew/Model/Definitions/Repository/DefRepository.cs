df95922b48fbf9a52f0543e26827e2f05811a3b0
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-09-22 12:27:24 +0300

    Add DefRepository

---
+++ Assets/PixelCrew/Model/Definitions/Repository/DefRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PixelCrew.Model.Definitions.Repository
+{
+    public class DefRepository<TDefType> : ScriptableObject where TDefType : IHaveId
+    {
+        [SerializeField] protected TDefType[] _collection;
+
+        public TDefType Get(string id)
+        {
+            foreach (var itemDef in _collection)
+            {
+                if (itemDef.Id == id)
+                    return itemDef;
+            }
+
+            return default;
+        }
+    }
+}
