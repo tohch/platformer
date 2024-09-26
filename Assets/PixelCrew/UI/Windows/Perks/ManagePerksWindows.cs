1e0e2752dbbb9dfbb6587e3e4cd4b4a26184a495
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-09-26 21:04:10 +0300

    PerkWidget

---
+++ Assets/PixelCrew/UI/Windows/Perks/ManagePerksWindows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Windows.Perks
+{
+    public class ManagePerksWindows : AnimatedWindow
+    {
+        [SerializeField] private Button _buyButton;
+        [SerializeField] private Button _useButton;
+    }
+}
