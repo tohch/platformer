a074308fab1aaeca0827b00f6d4336fc0d1c9018
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-09-27 21:55:45 +0300

    Prefab of UI ManagerPerks

--- Assets/PixelCrew/UI/Windows/Perks/PerkWidget.cs
+++ Assets/PixelCrew/UI/Windows/Perks/PerkWidget.cs
@@ -10,9 +10,18 @@ namespace PixelCrew.UI.Windows.Perks
 {
     public class PerkWidget : MonoBehaviour, IItemRenderer<string>
     {
+        [SerializeField] private GameObject _icon;
+        [SerializeField] private GameObject _isLocked;
+        [SerializeField] private GameObject _isUsed;
+        [SerializeField] private GameObject _isSelected;
         public void SetData(string data, int index)
         {
             throw new NotImplementedException();
+        }
+
+        public void OnSelected()
+        {
+
         }
     }
 }
