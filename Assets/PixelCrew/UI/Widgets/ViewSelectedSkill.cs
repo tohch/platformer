fa73a0bc8a8267e114da32de67b106ff291a1289
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-10-01 16:20:30 +0300

    Indecation Activity Skill

---
+++ Assets/PixelCrew/UI/Widgets/ViewSelectedSkill.cs
@@ -0,0 +1,42 @@
+﻿using PixelCrew.Model;
+using PixelCrew.Model.Definitions.Repositories;
+using PixelCrew.Model.Models;
+using PixelCrew.Utils.Disposables;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Widgets
+{
+    public class ViewSelectedSkill : MonoBehaviour
+    {
+        [SerializeField] private Image _skillIcon;
+        [SerializeField] private PerkRepository _perks;
+
+        private GameSession _session;
+        private readonly CompositeDisposable _trash = new CompositeDisposable();
+
+        void Start()
+        {
+            _session = FindObjectOfType<GameSession>();
+            _trash.Retain(_session.PerksModel.Subscribe(UpdateUsedSkillIcon));
+        }
+
+        private void UpdateUsedSkillIcon()
+        {
+            if (_session.PerksModel.Used == "") return;
+
+            var usedPerk = _session.PerksModel.Used;
+            var newSkillIcon = _perks.Get(usedPerk).Icon;
+            _skillIcon.sprite = newSkillIcon;
+            _skillIcon.color = new Color(1f, 1f, 1f, 1f);
+        }
+
+        public void OnDestroy()
+        {
+            _trash.Dispose();
+        }
+
+    }
+}
