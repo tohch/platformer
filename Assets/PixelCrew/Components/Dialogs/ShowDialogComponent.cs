d6f87b237aa2564719eea8c5bb5d9ae0450ad27b
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-10-27 18:45:04 +0300

    showdialog

--- Assets/PixelCrew/Components/Dialogs/ShowDialogComponent.cs
+++ Assets/PixelCrew/Components/Dialogs/ShowDialogComponent.cs
@@ -16,10 +16,29 @@ namespace PixelCrew.Components.Dialogs
         private DialogBoxController _dialogBox;
         public void Show()
         {
-            if (_dialogBox == null)
-                _dialogBox = FindObjectOfType<DialogBoxController>();
+            _dialogBox = FindDialogController();
 
             _dialogBox.ShowDialog(Data);
+        }
+
+        private DialogBoxController FindDialogController()
+        {
+            if (_dialogBox != null) return _dialogBox;
+
+            GameObject controllerGo;
+            switch (Data.Type)
+            {
+                case DialogType.Simple:
+                    controllerGo = GameObject.FindWithTag("SimpleDialog");
+                    break;
+                case DialogType.Personalized:
+                    controllerGo = GameObject.FindWithTag("PersonalizedDialog");
+                    break;
+                default:
+                    throw new ArgumentException("Undefined dialog type");
+            }
+
+            return controllerGo.GetComponent<DialogBoxController>();
         }
 
         public void Show(DialogDef def)
