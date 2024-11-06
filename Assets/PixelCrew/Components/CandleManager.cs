a97758982604126e5831e4342cf61d9f9372fc6c
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-11-06 16:01:00 +0300

    candle turn on

---
+++ Assets/PixelCrew/Components/CandleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class CandleManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _candle;
+        public GameObject Candle 
+        { 
+            get => _candle;
+            set => _candle = value; 
+        }
+        
+
+        public void TurnOn(GameObject go)
+        {
+            var candleManager = go.GetComponent<CandleManager>();
+            candleManager.Candle.SetActive(true);
+        }
+    }
+}
