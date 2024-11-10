f5e4e015bae533e12fba56d5281ca6cdae2b54f4
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-11-10 18:29:30 +0300

    LevelLoader

---
+++ Assets/PixelCrew/UI/LevelsLoader/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew.UI.LevelsLoader
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void OnFaterSceneLoade()
+        {
+            InitLoader();
+        }
+
+        private static LevelLoader Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private static void InitLoader()
+        {
+            if (Instance != null)
+                SceneManager.LoadScene("LevelLoader", LoadSceneMode.Additive);
+        }
+
+        public void Show(string sceneName)
+        {
+
+        }
+    }
+}
