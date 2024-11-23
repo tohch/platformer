e40ca9cdc9d95899017794f5abd4092f1e09dd32
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-11-23 12:23:04 +0300

    set post

---
+++ Assets/PixelCrew/Components/PostEffects/SetPostEffectProfile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PixelCrew.Components.PostEffects
+{
+    public class SetPostEffectProfile : MonoBehaviour
+    {
+        [SerializeField] private VolumeProfile _profile;
+        
+        public void Set()
+        {
+            var volumes = FindObjectsOfType<Volume>();
+            foreach (var volume in volumes)
+            {
+                if (!volume.isGlobal) continue;
+
+                volume.profile = _profile;
+                break;
+            }
+            
+        }
+    }
+}
