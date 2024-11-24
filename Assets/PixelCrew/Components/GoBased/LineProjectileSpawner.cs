ca2afe3cd6e761616c1080ba1eaa56a01797ea54
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-11-24 12:49:15 +0300

    === Spawner Tentacles ===

---
+++ Assets/PixelCrew/Components/GoBased/LineProjectileSpawner.cs
@@ -0,0 +1,40 @@
+﻿using PixelCrew.Creatures.Weapons;
+using PixelCrew.Utils;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.Components.GoBased
+{
+    public class LineProjectileSpawner : MonoBehaviour
+    {
+        [SerializeField] private LineProjectileSettings _settings;
+
+        public int BurstCount => _settings.BurstCount;
+        public void LaunchProjectiles()
+        {
+            var spawnPosition = new Vector3(_settings.PositionX, _settings.PositionY, transform.position.z);
+
+            SpawnUtils.Spawn(_settings.Prefab.gameObject, spawnPosition);
+            spawnPosition.x += _settings.DistanceBetvine;
+
+        }
+    }
+
+    [Serializable]
+    public struct LineProjectileSettings
+    {
+        [SerializeField] private GameObject _projectile;
+        [SerializeField] private float _positionY;
+        [SerializeField] private float _positionX;
+        [SerializeField] private float _distanceBetvine;
+        [SerializeField] private int _burstCount;
+        [SerializeField] private float _delay;
+
+        public GameObject Prefab => _projectile;
+        public int BurstCount => _burstCount;
+        public float PositionX => _positionX;
+        public float PositionY => _positionY;
+        public float DistanceBetvine => _distanceBetvine;
+    }
+}
---
+++ Assets/PixelCrew/Creatures/Mobs/Boss/BossSpawnTentacle.cs
@@ -0,0 +1,22 @@
+﻿using PixelCrew.Components.GoBased;
+using PixelCrew.Creatures.Mobs.Boss.Tentacles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PixelCrew.Creatures.Mobs.Boss
+{
+    public class BossSpawnTentacle : StateMachineBehaviour
+    {
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var tentacle = FindObjectsOfType<TentacleAI>();
+            var spawner = animator.GetComponent<LineProjectileSpawner>();
+            if (tentacle.Length < spawner.BurstCount)
+                spawner.LaunchProjectiles();
+        }
+    }
+}
