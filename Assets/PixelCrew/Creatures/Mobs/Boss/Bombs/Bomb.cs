d3bb9081935fc796b986abb966d78519a709a336
Kadantsev Anton <kadantsev.anton@yandex.ru>
2024-12-04 20:55:41 +0300

    bomb

---
+++ Assets/PixelCrew/Creatures/Mobs/Boss/Bombs/Bomb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Creatures.Mobs.Boss.Bombs
+{
+    public class Bomb : MonoBehaviour
+    {
+        [SerializeField] private float _ttl;
+        [SerializeField] private UnityEvent _onDetonate;
+        private Coroutine _coroutine;
+
+        private void OnEnable()
+        {
+            TryStop();
+            _coroutine = StartCoroutine(WaitAndDetonate());
+        }
+
+        private IEnumerator WaitAndDetonate()
+        {
+            yield return new WaitForSeconds(_ttl);
+            Detonate();
+            _coroutine = null;
+        }
+
+        public void Detonate()
+        {
+            _onDetonate?.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            TryStop();
+        }
+
+        private void TryStop()
+        {
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+    }
+}
